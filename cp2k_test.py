42d7ecee0faff92244b7b5e19229d935c8a6a528
Ole Schütt <oschuett@cp2k.org>
2015-04-09 14:51:59 +0200

    comply with pep8

--- cp2k_test.py
+++ cp2k_test.py
@@ -12,30 +12,32 @@ from ase.structure import molecule
 from ase.calculators.calculator import get_calculator
 from ase.units import GPa, Pascal, Hartree
 from ase.optimize import BFGS
-
 from cp2k import CP2K
 
-#===============================================================================
+
+#==============================================================================
 def test_H2_LDA():
-    calc = CP2K(label="test_H2_LDA")
+    calc = CP2K(label='test_H2_LDA')
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
-    energy = h2.get_potential_energy()/Hartree
-    diff = abs((energy + 0.932722287302)/energy)
+    energy = h2.get_potential_energy() / Hartree
+    diff = abs((energy + 0.932722287302) / energy)
     assert(diff < 1e-10)
-    print("passed test 'H2_LDA'")
+    print('passed test "H2_LDA"')
 
-#===============================================================================
+
+#==============================================================================
 def test_H2_PBE():
-    calc = CP2K(xc='PBE', label="test_H2_PBE")
+    calc = CP2K(xc='PBE', label='test_H2_PBE')
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
-    energy = h2.get_potential_energy()/Hartree
-    diff = abs((energy + 0.961680073441)/energy)
+    energy = h2.get_potential_energy() / Hartree
+    diff = abs((energy + 0.961680073441) / energy)
     assert(diff < 1e-10)
-    print("passed test 'H2_PBE'")
+    print('passed test "H2_PBE"')
 
-#===============================================================================
+
+#==============================================================================
 def test_H2_LS():
     inp = """&FORCE_EVAL
                &DFT
@@ -44,50 +46,54 @@ def test_H2_LS():
                  &END QS
                &END DFT
              &END FORCE_EVAL"""
-    calc = CP2K(label="test_H2_LS", inp=inp)
+    calc = CP2K(label='test_H2_LS', inp=inp)
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
-    energy = h2.get_potential_energy()/Hartree
-    diff = abs((energy + 0.932722212414)/energy)
+    energy = h2.get_potential_energy() / Hartree
+    diff = abs((energy + 0.932722212414) / energy)
     assert(diff < 1e-10)
-    print("passed test 'H2_LS'")
+    print('passed test "H2_LS"')
 
-#===============================================================================
+
+#==============================================================================
 def test_O2():
-    calc = CP2K(label="test_O2")
+    calc = CP2K(label='test_O2')
     o2 = molecule('O2', calculator=calc)
     o2.center(vacuum=2.0)
-    energy = o2.get_potential_energy()/Hartree
-    diff = abs((energy + 29.669802288970264)/energy)
+    energy = o2.get_potential_energy() / Hartree
+    diff = abs((energy + 29.669802288970264) / energy)
     assert(diff < 1e-10)
-    print("passed test 'O2'")
+    print('passed test "O2"')
 
-#===============================================================================
+
+#==============================================================================
 def test_restart():
     calc = CP2K()
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
     h2.get_potential_energy()
-    calc.write("test_restart") # write a restart
-    calc2 = CP2K(restart="test_restart") # load a restart
+    calc.write('test_restart')  # write a restart
+    calc2 = CP2K(restart='test_restart')  # load a restart
     assert not calc2.calculation_required(h2, ['energy'])
-    print("passed test 'restart'")
+    print('passed test "restart"')
 
-#===============================================================================
+
+#==============================================================================
 def test_geopt():
-    calc = CP2K(label="test_geopt")
+    calc = CP2K(label='test_geopt')
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
     dyn = BFGS(h2)
     dyn.run(fmax=0.05)
     dist = h2.get_distance(0, 1)
-    diff = abs(dist - 1.36733746519)
+    diff = abs((dist - 1.36733746519) / dist)
     assert(diff < 1e-10)
-    print("passed test 'geopt'")
+    print('passed test "geopt"')
 
-#===============================================================================
+
+#==============================================================================
 def main():
-    CP2K.command = "mpiexec -np 2 ./cp2k_shell.pdbg"
+    CP2K.command = 'mpiexec -np 2 ./cp2k_shell.pdbg'
 
     test_H2_LDA()
     test_H2_PBE()
